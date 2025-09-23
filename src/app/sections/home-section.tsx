91047997bdf639ceeb5e222f5924e2daaf38fb4c
Daniel Abnes <dkabnes@gmail.com>
2025-09-23 21:49:38 +0000

    move it to top right corner of circular image and just keep 14+ years in

--- src/app/sections/home-section.tsx
+++ src/app/sections/home-section.tsx
@@ -70,10 +70,10 @@ export default function HomeSection() {
                   priority
                 />
                 
-                <div className="absolute bottom-4 -left-4">
+                <div className="absolute top-4 -right-4">
                     <div className="p-4 bg-background/50 backdrop-blur-sm rounded-xl flex items-center gap-3 border shadow-lg">
                         <p className="text-4xl font-bold text-primary">14+</p>
-                        <p className="text-sm text-foreground/80 leading-tight">Years of<br/>Experience</p>
+                        <p className="text-sm text-foreground/80 leading-tight">Years</p>
                     </div>
                 </div>
 
