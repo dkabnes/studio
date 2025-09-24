e4ecbc93b1fdd0cfc664ce8ca889b07a7c00b3ec
Daniel Abnes <dkabnes@gmail.com>
2025-09-24 22:01:36 +0000

    Can you move the profile picture a little to the right horizontally

--- src/app/sections/home-section.tsx
+++ src/app/sections/home-section.tsx
@@ -76,7 +76,7 @@ export default function HomeSection() {
                             data-ai-hint={profileImage.imageHint}
                             width={400}
                             height={400}
-                            className="relative object-cover object-[10px_10px] w-full h-full scale-[1.3]"
+                            className="relative object-cover object-[25px_10px] w-full h-full scale-[1.3]"
                             priority
                         />
                     </div>
